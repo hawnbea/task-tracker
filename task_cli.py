81cfb058b57f4443c98e2e22e2767bd3de28470f
Hanbi Park <hanbipark53@gmail.com>
2026-04-24 17:07:19 -0400

    not much

---
+++ task_cli.py
@@ -0,0 +1,13 @@
+import json
+import sys
+
+def add_task(description):
+    tasks = load_tasks()
+
+    new_task = {
+        "id" : 
+        "description": desciprition,
+        "status" : "todo",
+        "createdAt" : now
+        "updatedAt" : now
+    }
